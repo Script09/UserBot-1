5b2f848457e98664f7f6769a3e76a393eb1b128c
rakeshsixnine <53681252+rakeshsixnine@users.noreply.github.com>
2019-08-04 10:47:49 +0530

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -16,12 +16,12 @@ from userbot import (COUNT_PM, CMD_HELP, BOTLOG, BOTLOG_CHATID,
 from userbot.events import register
 
 # ========================= CONSTANTS ============================
-UNAPPROVED_MSG = ("`Hello`\n\n"
-                  "`Sorry I'm not in a condition of helping anyone so it isn't worth messaging me.`\n"
-                  "`Instead, go to Google or` @CraBinz `and find solutions.`\n\n"
-                  "`Trust me you will discover more than what you think...\n`"
+UNAPPROVED_MSG = ("`Hello! This is a bot.`\n\n"
+                  "`My master hasn't approved you to PM.`\n"
+                  "`Please wait for my master to look in, he mostly approves PMs.`\n\n"
+                  "`As far as I know, he doesn't usually approve retards though.\n`"
                   "`Regards,`\n"
-                  "`Spyder :)`")
+                  "`Rakesh :)`")
 # =================================================================
 
 
