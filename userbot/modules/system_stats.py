f141db0be5dd6d118ab57f2d842073b2560ac99a
rakeshsixnine <53681252+rakeshsixnine@users.noreply.github.com>
2019-08-04 10:49:50 +0530

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -116,8 +116,8 @@ async def amireallyalive(alive):
     if not alive.text[0].isalpha() and alive.text[0] not in ("/", "#", "@", "!"):
         await alive.edit(
             "`"
-           "Guruji hum abhi zinda hai ;) \n\n"
-            f"Owner: Pero Sar @Spyderzzz \n"
+           "Yeah Noob! I am Alive 😛 ;) \n\n"
+            f"Owner: Noob @stranger667 \n"
             "`"
             )
 
